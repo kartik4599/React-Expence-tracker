0f44fd02c47898d98574e3926bc95ae3047a9d50
devloper-kartik <kartikmendu2@gmail.com>
2022-10-19 19:50:21 +0530

    Get Profile data

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../Context/auth-context";
 import classes from "./Profile.module.css";
@@ -7,6 +7,39 @@ const Profile = () => {
   const cxt = useContext(AuthContext);
   const nameRef = useRef();
   const photoRef = useRef();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const val = {
+          idToken: cxt.id,
+        };
+
+        const res = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyA9lLIJ70sceZJlxaMyrLjucZ-tMTKALK0",
+          {
+            method: "POST",
+            body: JSON.stringify(val),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (res.ok) {
+          const data = await res.json();
+          nameRef.current.value = data.users[0].displayName;
+          photoRef.current.value = data.users[0].photoUrl;
+        } else {
+          const data = await res.json();
+          alert(data.error.message);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getData();
+  }, [cxt.id]);
 
   const submitHandler = async (e) => {
     e.preventDefault();
